1e9e6f76a258b348b8703c30d48bdac61618faa5
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-08-21 01:27:33 +0500

    cors issue fixed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const express = require('express')
 const router = express.Router()
 
 
-router.get('/',(req,res,next)=>{
+router.get('/test',(req,res,next)=>{
+    console.log('hello')
     res.send('Joust testing')
 })
 
--- server.js
+++ server.js
@@ -10,20 +10,22 @@ const errorHandler = require('./middleware/errorHandler')
 const db = require('./database/index')
 const {PORT,BACKEND_SERVER_PATH}= require('./config/index')
 const router = require('./routes')
- const io = new Server(server)
+ const io = new Server(server,{
+    cors: {
+      origin: "http://localhost:3000"
+    }
+  })
  
 db()
- io.on('connection',(socket)=>{
-     console.log('a connection established')
-    })
-
 app.use(cors())
+app.use(express.json())
 app.use(router)
+io.on('connection',(socket)=>{
+    console.log('a connection established')
+   })
 app.use(errorHandler)
 server.listen(PORT,()=>{
-console.log('server is listening on port '+PORT+ ' at '+BACKEND_SERVER_PATH)
+    console.log('server is listening on port '+PORT+ ' at '+BACKEND_SERVER_PATH)
 })
-
-
 
 
